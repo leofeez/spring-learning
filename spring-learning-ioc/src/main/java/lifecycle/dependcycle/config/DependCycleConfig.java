9e6bf5e52ab2c0ba845f6d6861c426fa906e8c5d
lixiufei <mr.leofee@outlook.com>
2020-09-02 13:38:50 +0800

    测试循环依赖

--- spring-learning-ioc/src/main/java/lifecycle/dependcycle/config/DependCycleConfig.java
+++ spring-learning-ioc/src/main/java/lifecycle/dependcycle/config/DependCycleConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 /**
+ * 测试循环依赖
+ *
  * @author leofee
  */
 @ComponentScan(basePackages = "lifecycle.dependcycle")

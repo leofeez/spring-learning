ca73005f6e2eac5f6401c85c53ef7a78a64c3ed6
lixiufei <mr.leofee@outlook.com>
2020-11-26 18:17:24 +0800

    feat(ioc): 静态configuration内部类

--- spring-learning-annotation/src/main/java/anno/autowired/config/AutowiredConfig.java
+++ spring-learning-annotation/src/main/java/anno/autowired/config/AutowiredConfig.java
@@ -4,6 +4,7 @@ import anno.autowired.dao.StudentDao;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
 @ComponentScan(basePackages = {"anno.autowired.*"})
 @Configuration
@@ -19,4 +20,17 @@ public class AutowiredConfig {
         return studentDao;
     }
 
+    @Component
+    private static class NestedAutowiredConfig {
+
+        @Bean
+        public StudentDao getDao2() {
+            System.out.println("---------start----------");
+            System.out.println("通过 Nested @Bean 方式生成" + StudentDao.class.getName() + "实例");
+            StudentDao studentDao = new StudentDao();
+            studentDao.setFlag(2);
+            System.out.println("--------- end ----------");
+            return studentDao;
+        }
+    }
 }
